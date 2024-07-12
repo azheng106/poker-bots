111fb416bcf82d74af4467bea6b563b39a9781b9
azheng106 <azheng106@gmail.com>
2024-07-11 22:29:22 -0400

    a

---
+++ cmake-build-debug/CMakeFiles/PokerBots.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PokerBots.dir/main.cpp.obj"
+  "CMakeFiles/PokerBots.dir/main.cpp.obj.d"
+  "PokerBots.exe"
+  "PokerBots.exe.manifest"
+  "PokerBots.pdb"
+  "libPokerBots.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PokerBots.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
