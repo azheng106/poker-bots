d504ab37af5a497abca2c25deebdbbeaef9461e8
azheng106 <azheng106@gmail.com>
2024-07-14 14:20:04 -0400

    fix distributeCards function

---
+++ misc/cmake-build-debug/CMakeFiles/poker_bots.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/azhen/CLionProjects/poker-bots/CardUtil.cpp" "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/CardUtil.cpp.obj" "gcc" "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/CardUtil.cpp.obj.d"
+  "C:/Users/azhen/CLionProjects/poker-bots/classes/Card.cpp" "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Card.cpp.obj" "gcc" "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Card.cpp.obj.d"
+  "C:/Users/azhen/CLionProjects/poker-bots/classes/Game.cpp" "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Game.cpp.obj" "gcc" "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Game.cpp.obj.d"
+  "C:/Users/azhen/CLionProjects/poker-bots/classes/Player.cpp" "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Player.cpp.obj" "gcc" "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Player.cpp.obj.d"
+  "C:/Users/azhen/CLionProjects/poker-bots/classes/Round.cpp" "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Round.cpp.obj" "gcc" "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Round.cpp.obj.d"
+  "C:/Users/azhen/CLionProjects/poker-bots/main.cpp" "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/main.cpp.obj" "gcc" "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ misc/cmake-build-debug/CMakeFiles/poker_bots.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/CardUtil.cpp.obj"
+  "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/CardUtil.cpp.obj.d"
+  "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Card.cpp.obj"
+  "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Card.cpp.obj.d"
+  "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Game.cpp.obj"
+  "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Game.cpp.obj.d"
+  "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Player.cpp.obj"
+  "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Player.cpp.obj.d"
+  "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Round.cpp.obj"
+  "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/classes/Round.cpp.obj.d"
+  "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/main.cpp.obj"
+  "CMakeFiles/poker_bots.dir/C_/Users/azhen/CLionProjects/poker-bots/main.cpp.obj.d"
+  "libpoker_bots.dll.a"
+  "poker_bots.exe"
+  "poker_bots.exe.manifest"
+  "poker_bots.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poker_bots.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
