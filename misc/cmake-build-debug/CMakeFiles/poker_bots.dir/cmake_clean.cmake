4f6a3f8b4821336da54b9752e73e64ec44a890a5
Jaden129 <jkding129@outlook.com>
2024-07-23 10:09:52 -0400

    add cmake-build-debug

---
+++ misc/cmake-build-debug/CMakeFiles/poker_bots.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poker_bots.dir/C_/CS_Projects/poker-bots/classes/Card.cpp.obj"
+  "CMakeFiles/poker_bots.dir/C_/CS_Projects/poker-bots/classes/Card.cpp.obj.d"
+  "CMakeFiles/poker_bots.dir/C_/CS_Projects/poker-bots/classes/CardUtil.cpp.obj"
+  "CMakeFiles/poker_bots.dir/C_/CS_Projects/poker-bots/classes/CardUtil.cpp.obj.d"
+  "CMakeFiles/poker_bots.dir/C_/CS_Projects/poker-bots/classes/Game.cpp.obj"
+  "CMakeFiles/poker_bots.dir/C_/CS_Projects/poker-bots/classes/Game.cpp.obj.d"
+  "CMakeFiles/poker_bots.dir/C_/CS_Projects/poker-bots/classes/Player.cpp.obj"
+  "CMakeFiles/poker_bots.dir/C_/CS_Projects/poker-bots/classes/Player.cpp.obj.d"
+  "CMakeFiles/poker_bots.dir/C_/CS_Projects/poker-bots/main.cpp.obj"
+  "CMakeFiles/poker_bots.dir/C_/CS_Projects/poker-bots/main.cpp.obj.d"
+  "bin/poker_bots.exe"
+  "bin/poker_bots.exe.manifest"
+  "bin/poker_bots.pdb"
+  "libpoker_bots.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poker_bots.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
