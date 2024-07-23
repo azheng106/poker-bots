3ab2d48b5fdd2bac202494467bc27cd2f26f4053
azheng106 <azheng106@gmail.com>
2024-07-23 00:23:43 -0400

    work on making sfml function, got # error messages down to 3

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include "classes/Game.h"
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
 int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+    while (window.isOpen()) {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
     Game game;
     game.setupPlayers();
     while (!game.isFinished) {
